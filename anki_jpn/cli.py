672ed3b6dcb5d4cd8ab16710ff0d0350b721ce1b
Nathan Imse <njimse@gmail.com>
2024-12-11 20:08:26 -0800

    prettified the formatting of the cards and also fixed a bug that led to duplicated cards

--- anki_jpn/cli.py
+++ anki_jpn/cli.py
@@ -1,11 +1,11 @@
 import os
 import copy
 import argparse
-import itertools
 import anki.collection
 import genanki
 
 from anki_jpn.verbs import generate_forms, godan_stem_mapping, VerbClass
+from anki_jpn.util import delta_split
 
 CARD_CSS = """.card {
   font-family: arial;
@@ -18,7 +18,20 @@ CARD_CSS = """.card {
 .win .jp { font-family: "MS Mincho", "ＭＳ 明朝"; }
 .mac .jp { font-family: "Hiragino Mincho Pro", "ヒラギノ明朝 Pro"; }
 .linux .jp { font-family: "Kochi Mincho", "東風明朝"; }
-.mobile .jp { font-family: "Hiragino Mincho ProN"; }"""
+.mobile .jp { font-family: "Hiragino Mincho ProN"; }
+table {
+border: 1px solid black;
+border-collapse: collapse;
+}
+td {
+border: 1px solid black;
+}
+table td{
+padding: 10px;
+}
+.ending {color: red}
+.nightMode .ending {color: red}
+.nightMode table tr td {border: 1px solid white}"""
 
 def generate_verb_notes(model, deck, note, verb_class):
     expression, meaning, reading = note.values()
@@ -26,13 +39,14 @@ def generate_verb_notes(model, deck, note, verb_class):
     if expression[-1] in godan_stem_mapping.keys():
         known_forms = generate_forms(reading, verb_class)
         for conjugation, form, formality in known_forms:
+            conj_base, conj_ending = delta_split(reading, conjugation)
             formality_str = formality.value if formality is not None else ''
             all_tags = copy.deepcopy(note.tags)
             if formality_str:
                 all_tags.append(formality_str)
             all_tags.append(form.value.replace(' ', '-'))
             new_note = genanki.Note(model=model, fields=[
-                conjugation, expression, meaning, reading, formality_str, form.value
+                expression, meaning, reading, formality_str, form.value, conj_base, conj_ending
             ], tags=all_tags)
             deck.add_note(new_note)
 
@@ -46,26 +60,26 @@ def main(args):
     deck = anki.collection.Collection(args.input)
 
     new_model = genanki.Model(args.model_id, args.model_name, css=CARD_CSS)
-    new_model.set_fields([{"name": "conjugation"},
-                          {"name": "expression"},
+    new_model.set_fields([{"name": "expression"},
                           {"name": "meaning"},
                           {"name": "reading"},
                           {"name": "formality"},
-                          {"name": "form"}
+                          {"name": "form"},
+                          {"name": "conjugation base"},
+                          {"name": "conjugation ending"}
                           ])
     new_model.set_templates([
         {"name": "JapaneseConjugation",
-         "qfmt": '<div class=jp>{{expression}}<div class=jp><br><table><tr><td align="right">Formality:</td><td align="left">{{formality}}</td></tr><tr><td align="right">Form:</td><td align="left">{{form}}</td></tr></table>',
-         "afmt": "{{FrontSide}}<hr id=answer><div class=jp>{{furigana:conjugation}}</div><br>{{meaning}}"}
+         "qfmt": '<br><table align="center"><tr><td colspan=2><div class=jp>{{expression}}</div></td></tr><tr><td align="right">Formality</td><td align="left">{{formality}}</td></tr><tr><td align="right">Form</td><td align="left">{{form}}</td></tr></table>',
+         "afmt": "{{FrontSide}}<br><div class=jp>{{furigana:conjugation base}}<span class=ending>{{furigana:conjugation ending}}</span></div><br>{{meaning}}<br>"}
     ])
     new_deck = genanki.Deck(args.deck_id, args.deck_name)
 
     for verb_tag in [args.ichidan, args.godan, args.irregular]:
-        verb_ids = deck.find_cards(f"tag:{verb_tag}")
-        for card_id in verb_ids:
-            note = deck.get_card(card_id).note()
+        verb_ids = deck.find_notes(f"tag:{verb_tag}")
+        for note_id in verb_ids:
+            note = deck.get_note(note_id)
             generate_verb_notes(new_model, new_deck, note, verb_tag2class[verb_tag])
-
     outdir = os.path.dirname(os.path.abspath(args.output))
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
