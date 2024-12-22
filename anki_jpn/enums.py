666c1092383c9f252f913f97b502e529f21e3f3e
Nathan Imse <njimse@gmail.com>
2024-12-22 15:01:11 -0800

    updated the model generation to use separate note types for verbs and adjectives and also updated the CLI to use the new paradigm

--- anki_jpn/enums.py
+++ anki_jpn/enums.py
@@ -20,3 +20,32 @@ class VerbClass(Enum):
 class AdjectiveClass(Enum):
     NA = 'na-adjective'
     I = 'i-adjective'
+
+class ModelType(Enum):
+    VERB = 'verb'
+    ADJECTIVE = 'adjective'
+
+VERB_COMBOS = [
+    (Formality.POLITE, Form.NON_PAST),
+    (Formality.POLITE, Form.NON_PAST_NEG),
+    (Formality.POLITE, Form.PAST),
+    (Formality.POLITE, Form.PAST_NEG),
+    (Formality.POLITE, Form.VOLITIONAL),
+    (None, Form.TE),
+    (Formality.PLAIN, Form.NON_PAST),
+    (Formality.PLAIN, Form.NON_PAST_NEG),
+    (Formality.PLAIN, Form.PAST),
+    (Formality.PLAIN, Form.PAST_NEG)
+]
+
+ADJECTIVE_COMBOS = [
+    (Formality.POLITE, Form.NON_PAST),
+    (Formality.POLITE, Form.NON_PAST_NEG),
+    (Formality.POLITE, Form.PAST),
+    (Formality.POLITE, Form.PAST_NEG),
+    (None, Form.TE),
+    (Formality.PLAIN, Form.NON_PAST),
+    (Formality.PLAIN, Form.NON_PAST_NEG),
+    (Formality.PLAIN, Form.PAST),
+    (Formality.PLAIN, Form.PAST_NEG)
+]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,21 +1,28 @@
 import pytest
+from importlib import resources as impresources
+import js2py
+import anki_jpn.resources
 
-from anki_jpn.util import delta_split
-
-delta_split_data = [
-    ('foo', 'foobar', 'foo', 'bar'),
-    ('foo', 'bar', '', 'bar'),
-    ('foot', 'foobar', 'foo', 'bar'),
-    ('食べる', '食べます', '食べ', 'ます'),
-    ('食[た]べる', '食[た]べます', '食[た]べ', 'ます'),
-    ('来る', '来ない', '来', 'ない'),
-    ('来[く]る', '来[こ]ない', '', '来[こ]ない'),
-    ('歩[ある]く', '歩[ある]きます', '歩[ある]', 'きます'),
-    ('ある', 'ない', '', 'ない'),
-    ('食[た]べる', '食[た]べる', '食[た]べる', '')
+insert_ending_spans_file = impresources.files(anki_jpn.resources)/'insert_ending_spans.js'
+with insert_ending_spans_file.open('rt') as f:
+    insert_ending_spans_text = f.read()
+insert_ending_spans = js2py.eval_js(insert_ending_spans_text)
+insert_ending_spans_data = [
+    ('foo', 'foobar', 'foo<span class=ending>bar</span>'),
+    ('foo', 'bar', '<span class=ending>bar</span>'),
+    ('foo', 'foo', 'foo'),
+    ('foot', 'foobar', 'foo<span class=ending>bar</span>'),
+    ('食べる', '食べます', '食べ<span class=ending>ます</span>'),
+    ('<ruby><rb>食</rb><rt>た</rt></ruby>べる', '<ruby><rb>食</rb><rt>た</rt></ruby>べます',
+        '<ruby><rb>食</rb><rt>た</rt></ruby>べ<span class=ending>ます</span>'),
+    ('来る', '来ない', '来<span class=ending>ない</span>'),
+    ('<ruby><rb>来</rb><rt>く</rt></ruby>る', '<ruby><rb>来</rb><rt>こ</rt></ruby>ない',
+        '<ruby><rb>来</rb><rt><span class=ending>こ</span></rt></ruby><span class=ending>ない</span>'),
+    ('<ruby><rb>歩</rb><rt>ある</rt></ruby>く', '<ruby><rb>歩</rb><rt>ある</rt></ruby>きます', '<ruby><rb>歩</rb><rt>ある</rt></ruby><span class=ending>きます</span>'),
+    ('ある', 'ない', '<span class=ending>ない</span>'),
+    ('<ruby><rb>食</rb><rt>た</rt></ruby>べる', '<ruby><rb>食</rb><rt>た</rt></ruby>べる', '<ruby><rb>食</rb><rt>た</rt></ruby>べる')
 ]
-@pytest.mark.parametrize('dict_form, conj, ref_stem, ref_ending', delta_split_data)
-def test_delta_split(dict_form, conj, ref_stem, ref_ending):
-    stem, ending = delta_split(dict_form, conj)
-    assert stem == ref_stem
-    assert ending == ref_ending
+@pytest.mark.parametrize('dict_form, conj, ref_conjugation', insert_ending_spans_data)
+def test_insert_ending_spans(dict_form, conj, ref_conjugation):
+    modified_conjugation = insert_ending_spans(dict_form, conj)
+    assert modified_conjugation == ref_conjugation
