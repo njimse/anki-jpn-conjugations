1dca438157d5630b065cf3bab0949de4588a07b4
Nathan Imse <njimse@gmail.com>
2025-01-11 19:40:59 -0800

    changed styling so that the conjugation ending matches the formality color

--- anki_jpn/resources/insert_ending_spans.js
+++ anki_jpn/resources/insert_ending_spans.js
@@ -20,7 +20,7 @@ function insert_ending_spans(dict_form, conjugation) {
     var close_rt_tags = (conjugation.slice(split_index).match(/<\/rt>/g) || []).length;
     if (open_ruby_tags == close_ruby_tags) {
         // The split is not within a ruby tag, just use the split index as-is
-        new_conjugation = conjugation.slice(0,split_index) + "<span class=ending>" + conjugation.slice(split_index) + "</span>";
+        new_conjugation = conjugation.slice(0,split_index) + "<span id=ending>" + conjugation.slice(split_index) + "</span>";
     } else if (open_rt_tags == close_rt_tags) {
         // The split is inside a ruby tag, but outside of the rt tags. Assume the rt has changed, and keep
         // moving the split index until it is to the left of the current ruby section
@@ -31,13 +31,13 @@ function insert_ending_spans(dict_form, conjugation) {
                 break
             }
         }
-        new_conjugation = conjugation.slice(0,split_index) + "<span class=ending>" + conjugation.slice(split_index) + "</span>";
+        new_conjugation = conjugation.slice(0,split_index) + "<span id=ending>" + conjugation.slice(split_index) + "</span>";
     } else {
         // The split is inside an rt tag. Locate the closing rt tag so that we can insert a span within the rt
         var rt_close_offset = conjugation.slice(split_index).indexOf("</rt>");
-        var new_base = conjugation.slice(0, split_index) + "<span class=ending>" + conjugation.slice(split_index, split_index + rt_close_offset) + "</span>";
+        var new_base = conjugation.slice(0, split_index) + "<span id=ending>" + conjugation.slice(split_index, split_index + rt_close_offset) + "</span>";
         var ruby_close_offset = conjugation.slice(split_index+rt_close_offset).indexOf("</ruby>") + "</ruby>".length;
-        var new_ending = conjugation.slice(split_index + rt_close_offset, split_index+rt_close_offset + ruby_close_offset) + "<span class=ending>" + conjugation.slice(split_index+rt_close_offset + ruby_close_offset) + "</span>";
+        var new_ending = conjugation.slice(split_index + rt_close_offset, split_index+rt_close_offset + ruby_close_offset) + "<span id=ending>" + conjugation.slice(split_index+rt_close_offset + ruby_close_offset) + "</span>";
         new_conjugation = new_base + new_ending;
     }
     return new_conjugation;
